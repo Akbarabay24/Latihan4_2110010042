15a69750f2a93245a719a18049060bd99feb055c
unknown <akbarfurqoni24@gmail.com>
2023-06-13 06:57:20 -0700

    first commit

---
+++ src/latihan4/Latihan4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan4;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Latihan4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
